d6e24b4daf9e6c4298729265c4d649a63c4db56d
georg2990 <georg2990@gmail.com>
2025-09-04 19:20:38 +0200

    Tilføjet en Tivoli Ride

--- TivoliRide/src/TivoliRide.java
+++ TivoliRide/src/TivoliRide.java
@@ -1,2 +1,15 @@
 public class TivoliRide {
+
+    private double minHeight;
+
+    // Constructor to set the minimum height
+    public TivoliRide(double minHeight) {
+        this.minHeight = minHeight;
+    }
+
+    // We use boolean method to check if person is tall enough for the ride
+    public boolean canRide(double height) {
+        return height >= minHeight;
+
+    }
 }
